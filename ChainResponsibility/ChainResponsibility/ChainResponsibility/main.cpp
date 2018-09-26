f2f3d217797abd3f3e1663019a37bb0db004a358
fordreaming <fordreaming1@yeah.net>
2018-09-26 10:11:00 +0800

    chain of responsbility
    chain of responsbility pattern

---
+++ ChainResponsibility/ChainResponsibility/ChainResponsibility/main.cpp
@@ -0,0 +1,14 @@
+#include "ChainResponsibility.hpp"
+
+//测试案例
+int main()
+{	
+	Manager *general = new GeneralManager(NULL, "A"); //设置上级，总经理没有上级
+	Manager *majordomo = new Majordomo(general, "B"); //设置上级
+	Manager *common = new CommonManager(majordomo, "C"); //设置上级
+	common->DealWithRequest("D",300);   //员工D要求加薪
+	common->DealWithRequest("E", 600);
+	common->DealWithRequest("F", 1000);
+	delete common; delete majordomo; delete general;
+	return 0;
+}
